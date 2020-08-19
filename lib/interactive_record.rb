2eeec91077bd5ff853ec684b1af3b39a546dc2d5
edmundo papi <edmundopierre@yahoo.com>
2020-08-19 19:14:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,13 +14,13 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
+    binding.pry
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
   end
-binding.pry
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
